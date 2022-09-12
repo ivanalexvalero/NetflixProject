2cf1a3d1901f1452ea20ef4a5fdb78d821e16947
ivan <ivanvalero1985@gmail.com>
2022-09-12 20:14:05 -0300

    Initial Commit

---
+++ NetflixProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixProject
+//
+//  Created by Ivan Valero on 12/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
